2ef7b2d0fcf4d9eeaadc166687a0276b1d05e063
RyuseiNomi <kanontonto1010@gmail.com>
2022-05-15 20:33:55 +0900

    adjust profile area

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'App.css';
 import 'antd/dist/antd.min.css';
+import { Row, Col } from 'antd';
 import TitleBar from 'components/TitleBar'
 import Thumbnail from 'components/Thumbnail'
 import ProfileDescription from 'components/ProfileDescription'
@@ -8,11 +9,19 @@ import ProfileDescription from 'components/ProfileDescription'
 function App() {
   return (
     <div className="App">
-      <header>
-        <TitleBar />
-        <Thumbnail />
-        <ProfileDescription />
-      </header>
+      <TitleBar />
+      <Row justify="center">
+        <p className="profile-title">ABOUT ME</p>
+      </Row>
+      <Row>
+        {/* TODO: タブレットサイズ以上の場合は横並びのレイアウトにする */}
+        <Col xs={24} sm={24} md={24} lg={24} >
+          <Thumbnail />
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={24} >
+          <ProfileDescription />
+        </Col>
+      </Row>
     </div>
   );
 }
--- src/components/Thumbnail/index.tsx
+++ src/components/Thumbnail/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './index.css';
+import { Row } from 'antd';  
+import { TwitterOutlined } from '@ant-design/icons';
 
 function Thumbnail() {
 
@@ -9,7 +11,10 @@ function Thumbnail() {
         <source type="image/webp" srcSet={`${process.env.PUBLIC_URL}/thumbnail.webp`} ></source>
         <img src={`${process.env.PUBLIC_URL}/thumbnail.jpeg`} alt="thumbnail" />
       </picture>
-      <p>とんと</p>
+      <Row justify="center">
+        <p>とんと</p>
+        <a href="https://twitter.com/27ma4_ton10" rel="noopener noreferrer" target="_blank"><TwitterOutlined /></a>
+      </Row>
     </div>
   );
 }
