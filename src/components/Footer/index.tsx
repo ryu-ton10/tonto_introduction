59a4bd6deb7092537cb2c2a415aa045ef32a9412
RyuseiNomi <kanontonto1010@gmail.com>
2022-05-30 09:25:46 +0900

    create Footer element

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function Footer() {
+
+  return (
+    <div className="footer">
+      <p>©︎2022    <a href="https://twitter.com/27ma4_ton10" rel="noopener noreferrer" target="_blank">@27ma4_ton10</a></p>
+    </div>
+  );
+};
+
+export default Footer;
