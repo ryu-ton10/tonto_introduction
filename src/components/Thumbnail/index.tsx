35a4a8086f72003a01d4484b6cebaa023769944a
RyuseiNomi <kanontonto1010@gmail.com>
2022-06-03 12:25:24 +0900

    minor modification

--- src/components/Thumbnail/index.tsx
+++ src/components/Thumbnail/index.tsx
@@ -4,12 +4,12 @@ import './index.css';
 function Thumbnail() {
 
   return (
-    <div className="thumbnail-image">
+    <>
       <picture>
         <source type="image/webp" srcSet={`${process.env.PUBLIC_URL}/thumbnail.webp`} ></source>
         <img src={`${process.env.PUBLIC_URL}/thumbnail.jpeg`} alt="thumbnail" />
       </picture>
-    </div>
+    </>
   );
 }
 
