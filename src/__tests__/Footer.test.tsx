01e8deb028279eaaa55355509aac30e6744df9a7
RyuseiNomi <kanontonto1010@gmail.com>
2022-07-09 17:38:35 +0900

    revise to use createRoot when render component in test file

--- src/__tests__/Footer.test.tsx
+++ src/__tests__/Footer.test.tsx
@@ -1,20 +1,23 @@
-import { render, unmountComponentAtNode } from 'react-dom';
+import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { act } from 'react-dom/test-utils';
 import renderer from 'react-test-renderer';
 import Footer from './../components/Footer';
 
 let container = null;
+let root = null;
 const sample_twitter_url = "http://sample_twitter_url.hoge";
 
 beforeEach(() => {
   // container の定義
   container = document.createElement("div");
   document.body.appendChild(container);
+  root = createRoot(container);
 });
 
 afterEach(() => {
   // 定義した container の除去
-  unmountComponentAtNode(container);
+  root.unmount(container);
   container.remove();
   container = null;
 });
@@ -30,10 +33,10 @@ it('Footer が表示されていること', () => {
 // ================ act tests ====================
 it('Footer に西暦と twitter id が表示されていること', () => {
   act(() => {
-    render(<Footer twitter_url={sample_twitter_url} />, container);
+    root.render(<Footer twitter_url={sample_twitter_url} />);
   })
 
   const displayed = document.querySelector("[data-testid=footer]");
-  expect(displayed.textContent).toBe("©︎2022    @27ma4_ton10");
+  expect(displayed.textContent).toBe("Privacy Policy©︎2022    @27ma4_ton10");
   console.log(displayed.textContent);
 });
