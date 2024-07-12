b6e802b94e39b000969e34b3dffd41116a02f3a7
RyuseiNomi <kanontonto1010@gmail.com>
2024-07-12 19:23:37 +0900

    replace thumbnail icon

--- src/components/Thumbnail/index.tsx
+++ src/components/Thumbnail/index.tsx
@@ -6,8 +6,8 @@ function Thumbnail() {
   return (
     <div className="thumbnail-image" aria-label="thumbnail-wrapper">
       <picture>
-        <source type="image/webp" srcSet={`${process.env.PUBLIC_URL}/thumbnail.webp`} ></source>
-        <img src={`${process.env.PUBLIC_URL}/thumbnail.jpeg`} alt="thumbnail" />
+        <source type="image/webp" srcSet={`${process.env.PUBLIC_URL}/assets/icon.webp`} ></source>
+        <img src={`${process.env.PUBLIC_URL}/assets/icon.jpeg`} alt="thumbnail" />
       </picture>
     </div>
   );
