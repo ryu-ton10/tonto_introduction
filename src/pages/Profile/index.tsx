7329e0b7cf39ff36174b033d254b7a84a87872bf
RyuseiNomi <ryusei@RyuseiNominoMac-mini.local>
2025-12-28 14:35:46 +0900

    add sns links to profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,15 @@
 import { useEffect } from 'react';
+import { AiFillYoutube } from 'react-icons/ai';
 import './index.css';
 
 function Profile() {
+
+  const twitterUrl = "https://x.com/27ma4_ton10_v1"
+  const blueskyUrl = "https://bsky.app/profile/ton10.bsky.social"
+  const noteUrl = "https://note.com/kanon_tonto"
+  const githubUrl = "https://github.com/ryu-ton10"
+  const youtubeUrl = "https://www.youtube.com/@kanon_tonto"
+  const youtubeSubscribeUrl = "https://www.youtube.com/@kanon_tonto?sub_confirmation=1"
 
   const toggleFadeIn = () => {
     const currentScrollY = window.scrollY;
@@ -27,6 +35,7 @@ function Profile() {
     <div className="profile">
       <p className="profile_title">PROFILE</p>
       <div className="profile_content">
+
         <div className="profile_content-thumbnail" aria-label="thumbnail-wrapper">
           <picture className="profile_content-thumbnail-image">
             <source type="image/webp" srcSet={`${process.env.PUBLIC_URL}/assets/about_me/thumbnail.webp`} />
@@ -37,7 +46,9 @@ function Profile() {
             <img src={`${process.env.PUBLIC_URL}/assets/about_me/name.png`} alt="thumbnail" />
           </picture>
         </div>
+
         <div className="profile_content-gallery">
+
           <div className="profile_content-gallery-standing">
             <picture>
               <source type="image/webp" srcSet={`${process.env.PUBLIC_URL}/assets/about_me/standing_paka.webp`} />
@@ -53,6 +64,70 @@ function Profile() {
               <source type="image/webp" srcSet={`${process.env.PUBLIC_URL}/assets/about_me/gallery_illust.webp`} />
               <img src={`${process.env.PUBLIC_URL}/assets/about_me/gallery_illust.png`} alt="thumbnail" />
             </picture>
+          </div>
+
+          <div className="profile_content-gallery-links">
+
+            <div className="profile_content-gallery-links-account">
+              <a href={twitterUrl} rel="noopener noreferrer" target="_blank">
+                <img src="assets/about_me/x_logo.png" alt="x-icon" />
+                <div>
+                  <p className="profile_content-gallery-links-name">嘉音 とんと@ものづくり系VTuber</p>
+                  <p className="profile_content-gallery-links-id">@27ma4_ton10_v1</p>
+                </div>
+              </a>
+            </div>
+            <div className="profile_content-gallery-links-account">
+              <a href={blueskyUrl} rel="noopener noreferrer" target="_blank">
+                <img src="assets/about_me/bluesky_logo.png" alt="bluesky-icon" />
+                <div>
+                  <p className="profile_content-gallery-links-name">嘉音 とんと@ものづくり系VTuber</p>
+                  <p className="profile_content-gallery-links-id">@ton10.bsky.social</p>
+                </div>
+              </a>
+            </div>
+            <div className="profile_content-gallery-links-account">
+              <a href={noteUrl} rel="noopener noreferrer" target="_blank">
+                <img src="assets/about_me/note_logo.png" alt="note-icon" />
+                <div>
+                  <p className="profile_content-gallery-links-name">嘉音 とんと | note</p>
+                  <p className="profile_content-gallery-links-id">note.com</p>
+                </div>
+              </a>
+            </div>
+            <div className="profile_content-gallery-links-account">
+              <a href={githubUrl} rel="noopener noreferrer" target="_blank">
+                <img src="assets/about_me/github_logo.png" alt="github-icon" />
+                <div>
+                  <p className="profile_content-gallery-links-name">tonto | Github</p>
+                  <p className="profile_content-gallery-links-id">github.com</p>
+                </div>
+              </a>
+            </div>
+            <div className="profile_content-gallery-links-youtube">
+              <div className="profile_content-gallery-links-youtube-button">
+                <a href={youtubeUrl} rel="noopener noreferrer" target="_blank">
+                  <div className="youtube-icon"><AiFillYoutube /></div>
+                </a>
+                <a href={youtubeSubscribeUrl} rel="noopener noreferrer" target="_blank">
+                  <p>登録する</p>
+                </a>
+              </div>
+              <div className="profile_content-gallery-links-youtube-thumbnail">
+                <div>
+                  <img src="assets/about_me/youtube_thumbnail_1.png" alt="youtube-thumbnail" />
+                </div>
+                <div>
+                  <img src="assets/about_me/youtube_thumbnail_2.png" alt="youtube-thumbnail" />
+                </div>
+                <div>
+                  <img src="assets/about_me/youtube_thumbnail_3.png" alt="youtube-thumbnail" />
+                </div>
+                <div>
+                  <img src="assets/about_me/youtube_thumbnail_4.png" alt="youtube-thumbnail" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
